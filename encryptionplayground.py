8ef0fe37cd99d460ac350f0a8bb3ed0967807c62
Calvin Tate <57345564+Calvonator@users.noreply.github.com>
2020-06-20 13:21:09 +0800

    Update encryptionplayground.py
    nEW MENU OPTIONS FOR RANDMOM NUMBER ADDED

--- encryptionplayground.py
+++ encryptionplayground.py
@@ -1,8 +1,14 @@
 #This is the main file for the encryption playground where each encrytpion cipher is placed into its own file and imported here 
 import caesar
+import lsfr
+import binaryConv as conv
+import random
 
 print("-------------------------------------------------------\nWelcome to the Encryption Playground!\nPlease choose fromt the following encryption schemes:\n")
 print("[1] Caesar Cipher")
+print("[2] LSFR Psuedo-Random Number Generation")
+
+
 
 while True:
     choice = input("Please choose from the menu options: ")
@@ -15,13 +21,66 @@ while True:
             cipher = caesar.encrypt(plaintext, key)
             print(cipher)
             choice = input("Would you like to decrpy the message? (Y or N) ")
+            
             if choice.lower() == 'y':
                 plaintext = caesar.decrypt(cipher, key)
                 print(plaintext)
                 
-            
+    elif choice == '2':
         
+        ffSeed = []
+        switch = []
+        
+        print("Please enter the values for the flip flop boxes individually: (0 or 1 or q)\n")
+        
+        while True:
+            
+            bit = input()
+            
+            if bit.lower() == 'q':
+                break
+            
+            ffSeed.append(int(bit))
 
+        randChoice = input("Would you like to input the switches manually (y) or randomly generate (n)?")
+        
+        if randChoice.lower() == 'y':
+            
+            for _ in range(len(ffSeed)):
+                
+                bit = input()
+                
+                if bit.lower() == 'q':
+                    break
+                
+                ffSeed.append(int(bit))
+                
+        elif randChoice.lower() == 'n':
+            
+            for x in range(len(ffSeed)):
+                
+                switch.append(random.randint(0, 1))
+                
+        numbers = []
+        
+        while True:
+            
+            randNumber = []
+            
+            for _ in range(8):
+            
+                rand = lsfr.shiftFeedback(ffSeed, switch) 
+                randNumber.append(rand)    
+                
+            print(conv.unsignedDecimalEncode(randNumber))
+
+            numbers.append(conv.unsignedDecimalEncode(randNumber)) 
+            again = input("Generate another number?")
+
+            if again.lower() == 'n':
+                break
+            
+        print(numbers)
+        
     elif choice.lower() == 'q':
         break
-        
